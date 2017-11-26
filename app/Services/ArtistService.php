f12cc25ed3490f96b7b9d40bc13567c1ae06638c
PieterjanDeClippel <pieterjandeclippel@msn.com>
2017-11-26 01:02:34 +0100

    Updated ArtistService: working state of active/left members of an artist

--- app/Services/ArtistService.php
+++ app/Services/ArtistService.php
@@ -42,25 +42,20 @@ class ArtistService {
         $artist->user_update = Auth::user()->id;
         $artist->save();
         
-        // One would expect this to work (sync m2m, for members set active=TRUE, for past_members set active=FALSE)
-        // first detach existing pivots which are not in the members-list, and attach (if necessary) the remaining members. set all "actives" to TRUE
-        $artist->members()->sync($artistData->members, array('active' => TRUE));
-        // keep the above set pivots, and attach (if necessary) the past_members, setting all "actives" to FALSE
-        $artist->members()->syncWithoutDetaching($artistData->past_members, array('active' => FALSE));
-        
-        // This on the other hand works, but you'd have to add multiple conditions to make it work without getting bugs
-        // Which would be extremely annoying
-        //$merged_members = array_merge($artistData->members, $artistData->past_members);
-        //$actives = array_merge(
-        //    array_fill(0, count($artistData->members), ['active' => true]),
-        //    array_fill(0, count($artistData->past_members), ['active' => false])
-        //);
-        //$syncData = array_combine($merged_members,$actives);
-        //$artist->members()->sync($syncData);
+        // set members to active or not
+        $data_to_sync = array();
+        foreach($artistData->members as $member){
+            $pivot_data = ['active' => TRUE];
+            $data_to_sync[$member] = $pivot_data;
+        }
+        foreach($artistData->past_members as $member){
+            $pivot_data = ['active' => FALSE];
+            $data_to_sync[$member] = $pivot_data;
+        }
+        $artist->members()->sync($data_to_sync);
         
         // Delete old entities
         Medium::where('subject_id',$artist->subject->id)->delete();
-        
         foreach($artistData->media as $mediumData){
             $type = MediumType::find($mediumData->medium_type_id);
             $medium = new Medium();
