34814ae3f9f4b23467c545066e6ad196a17fd472
PieterjanDeClippel <pieterjandeclippel@msn.com>
2017-12-07 15:13:23 +0100

    Small change

--- resources/views/song/show.blade.php
+++ resources/views/song/show.blade.php
@@ -107,7 +107,6 @@
 @endsection
 
 @section('javascript')
-        if(){
   @if($song->subject->youtube_id !== null)
     var lines = {!! json_encode(array_values($lines)) !!};
     var times = {!! $song->latest_lyrics === null ? '[]' : json_encode($song->latest_lyrics->timing) !!};
